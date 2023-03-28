8267cba9a0fac3af133b01e59191059b8dec77d4
kane6032 <robertrodgers6032@outlook.com>
2023-03-28 10:29:01 -0400

    Fixed lab 11

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -15,39 +15,29 @@ class PyGameAIPlayer:
         pass
 
     def selectAction(self, state):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            
-            # Chooses a random city to go to and returns the corresponding key
-            choice = random.randint(1, 10)
-            choice -= 1
-            if choice == 0:
-                event.key = pygame.K_0
-            elif choice == 1:
-                event.key = pygame.K_1
-            elif choice == 2:
-                event.key = pygame.K_2
-            elif choice == 3:
-                event.key = pygame.K_3
-            elif choice == 4:
-                event.key = pygame.K_4
-            elif choice == 5:
-                event.key = pygame.K_5
-            elif choice == 6:
-                event.key = pygame.K_6
-            elif choice == 7:
-                event.key = pygame.K_7
-            elif choice == 8:
-                event.key = pygame.K_8
-            else:
-                event.key = pygame.K_9
-                # We can end the game here since we aren't adding code elsewhere to check for an game endstate
-                print("\n\n\tLegolas made it to the end! Quiting game...\n\n")
-                pygame.quit()
-            return event.key
-
-        return ord(str(state.current_city))  # Not a safe operation for >10 cities
+        # Chooses a random city to go to and returns the corresponding key
+        choice = random.randint(1, 10)
+        choice -= 1
+        if choice == 0:
+            return pygame.K_0
+        elif choice == 1:
+            return pygame.K_1
+        elif choice == 2:
+            return pygame.K_2
+        elif choice == 3:
+            return pygame.K_3
+        elif choice == 4:
+            return pygame.K_4
+        elif choice == 5:
+            return pygame.K_5
+        elif choice == 6:
+            return pygame.K_6
+        elif choice == 7:
+            return pygame.K_7
+        elif choice == 8:
+            return pygame.K_8
+        else:
+            return pygame.K_9
 
 
 """ Create PyGameAICombatPlayer class here"""
@@ -55,17 +45,8 @@ class PyGameAICombatPlayer(CombatPlayer):
     def __init__(self, name):
         super().__init__(name)
 
-    def weapon_selecting_strategy(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                
-                choice = random.randint(1, 3)
-                self.weapon = choice - 1
-                return self.weapon
-                
-                    #if event.key in [ord("s"), ord("a"), ord("f")]:
-                    #    choice = {ord("s"): 1, ord("a"): 2, ord("f"): 3}[event.key]
-                    #    self.weapon = choice - 1
-                    #    return self.weapon
+    def weapon_selecting_strategy(self):               
+        choice = random.randint(1, 3)
+        self.weapon = choice - 1
+        return self.weapon
+
